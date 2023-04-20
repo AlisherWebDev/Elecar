4bb48640bd1a8f7effa06fab366fb7a947539a70
AlisherWebDev <alisherbozorov2001@gmail.com>
2023-04-20 22:28:44 +0500

    Header qismi tugadi Responsiv qismi bilan

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// NAVBAR MENU RESPONSIVE START
+let myNav = document.querySelector("#myNav");
+
+function openNav() {
+  myNav.style.display = "block";
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  myNav.style.display = "none";
+  document.getElementById("myNav").style.height = "0%";
+}
+// NAVBAR MENU RESPONSIVE END
