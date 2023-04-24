b3605ad7cb7c40e4ce187a69ffbcd0326d531b6c
AlisherWebDev <alisherbozorov2001@gmail.com>
2023-04-24 21:29:35 +0500

    Header va About sahifalari tayyor bo'ldi

--- js/index.js
+++ js/index.js
@@ -11,3 +11,32 @@ function closeNav() {
   document.getElementById("myNav").style.height = "0%";
 }
 // NAVBAR MENU RESPONSIVE END
+
+// carousel-card
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 2500,
+  autoplayHoverPause: true,
+  lazyLoad: true,
+  responsiveClass: true,
+  dotsEach: true,
+  dotsContainer: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    600: {
+      items: 3,
+      nav: false,
+    },
+    1000: {
+      items: 3.5,
+      nav: true,
+      loop: true,
+      dotsData: true,
+    },
+  },
+});
